403bb820935c7defadafa421e5c301da118757da
Jaden <jgoore2023@mka.org>
2018-08-22 15:25:17 -0400

    I created a button to create books and I also added the show action page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+
 	def index
 	  @books = Book.all.order('created_at DESC')
 	end
 
 	def new
 	  @book = Book.new
+	end
+
+	def show
 	end
 
 	def create
@@ -19,6 +24,9 @@ class BooksController < ApplicationController
 
 	  def book_params
 	  	params.require(:book).permit(:title, :description, :author)
+	  end
 
+	  def find_book
+	  	@book = Book.find(params[:id])
 	  end
 end
