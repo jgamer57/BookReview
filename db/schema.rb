bfe06a4d5047c2ece40f5583b104cdfb271b8253
Jaden <jgoore2023@mka.org>
2018-08-31 15:50:47 -0400

    installed paperclip gem addedimage uplod field to add book layout books on homepage. i#	new file:   public/uploads/book/image/14/book_index_comiciwannagrowupnew1.png

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_154525) do
+ActiveRecord::Schema.define(version: 2018_08_31_181537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2018_08_28_154525) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+    t.string "book_img_file_name"
+    t.string "book_img_content_type"
+    t.bigint "book_img_file_size"
+    t.datetime "book_img_updated_at"
+    t.string "image"
   end
 
   create_table "categories", force: :cascade do |t|
