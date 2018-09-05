ab957e0718392142e775d4c79360227f743097b0
Darius Goore <dariusgoore@gmail.com>
2018-09-05 09:33:37 -0400

    Adding name to user model
    adding reviewer name to reviews partial
    updating application controller for name attribute
    adding link to update user to header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_015651) do
+ActiveRecord::Schema.define(version: 2018_09_05_130130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,8 @@ ActiveRecord::Schema.define(version: 2018_09_02_015651) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
-    t.string "book_img_file_name"
-    t.string "book_img_content_type"
-    t.bigint "book_img_file_size"
-    t.datetime "book_img_updated_at"
     t.string "image"
+    t.string "asin"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -53,6 +50,8 @@ ActiveRecord::Schema.define(version: 2018_09_02_015651) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
