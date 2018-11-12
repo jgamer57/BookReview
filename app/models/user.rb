ee1884a4a44835cbd3ce7d05b644300b6939c1cc
Darius Goore <dariusgoore@gmail.com>
2018-11-12 11:36:02 -0500

    adding user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	 def adorable_avatar
+     "https://api.adorable.io/avatars/50/#{email}"
+	 end
 end
